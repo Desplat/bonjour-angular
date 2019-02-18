fe38a028f704fa26c5d2e158e81338cd9af95f14
Desplat <matt-desplat@wanadoo.fr>
2019-02-18 15:04:10 +0100

    Ajout des pages

--- src/app/liste-collegues-component/liste-collegues-component.component.ts
+++ src/app/liste-collegues-component/liste-collegues-component.component.ts
@@ -9,6 +9,28 @@ import { Collegue } from '../models';
 export class ListeColleguesComponentComponent implements OnInit {
 
   @Input() listCollegues: Collegue[];
+  debut: number = 0;
+  fin: number = 3;
+  focus: number = 1;
+
+  clic1() {
+
+    this.debut = 0;
+    this.fin = 3;
+    this.focus = 1;
+  }
+
+  clic2() {
+    this.debut = 3;
+    this.fin = 6;
+    this.focus = 2;
+  }
+
+  clic3() {
+    this.debut = 6;
+    this.fin = 9;
+    this.focus = 3;
+  }
 
   constructor() { }
 
