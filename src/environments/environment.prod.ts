2cce5ba31e50ccbe176273b64f7b0377a03c8100
Desplat <matt-desplat@wanadoo.fr>
2019-02-21 12:08:14 +0100

    Correction adresse prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   // ajout d'une URL backend en mode développement
-  backendUrl: 'https://desplat-top-collegues.herokuapp.com/collegues'
+  backendUrl: 'https://desplat-top-collegues.herokuapp.com'
 };
